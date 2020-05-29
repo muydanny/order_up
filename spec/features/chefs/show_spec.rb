37627a9305796ed4083efaa79ec8f05830255427
Danny Ramos <danramos21@gmail.com>
2020-05-29 10:22:00 -0600

    User story 3 spec pt 1

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "When I visit a chef's show page" do
+  before(:each) do
+    @chef = Chef.create(name: "Chef Sam")
+    @sandwich  = @chef.dishes.create(name: "BLT", description: "Delicious and hand made") 
+
+    @lettuce = Ingredient.create(name:"Lettuce", calories: 20)
+    @tomato = Ingredient.create(name:"Tomato", calories: 50)
+    @bacon = Ingredient.create(name:"Bacon", calories: 150)
+
+    DishIngredient.create(dish_id: @sandwich.id, ingredient_id: @lettuce.id)
+    DishIngredient.create(dish_id: @sandwich.id, ingredient_id: @tomato.id)
+    DishIngredient.create(dish_id: @sandwich.id, ingredient_id: @bacon.id)
+  end
+
+  it "I see the name of that chefAnd I see a link to view a list of all ingredients" do
+    visit "/chefs/#{@chef.id}"
+
+   
+  end
+end
+
+# As a visitor
+# When I visit a chef's show page
+# I see the name of that chef
+# And I see a link to view a list of all 
+# ingredients that this chef uses in their dishes
+# When I click on that link
+# I'm taken to a chef's ingredient index page
+# and I can see a unique list of names of all the 
+# ingredients that this chef uses
